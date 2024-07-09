e3fc517ce958b09eabaab2d5e5d2364ad98f42db
emman <emmanuelassifuah9@gmail.com>
2024-07-09 21:02:54 +0000

    setting up

---
+++ src/main/java/com/GTP_managemet_app_Backend/GTP_managemet_app_Backend/GtpManagemetAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.GTP_managemet_app_Backend.GTP_managemet_app_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtpManagemetAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtpManagemetAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GTP_managemet_app_Backend/GTP_managemet_app_Backend/GtpManagemetAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GTP_managemet_app_Backend.GTP_managemet_app_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtpManagemetAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
