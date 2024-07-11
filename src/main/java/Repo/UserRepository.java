ddf55248a42d6b009deaa5cba25a8e93149a164d
emman <emmanuelassifuah9@gmail.com>
2024-07-11 08:36:14 +0000

    modified some classes

--- src/main/java/Repo/UserRepository.java
+++ src/main/java/Repo/UserRepository.java
@@ -1,4 +1,8 @@
 package Repo;
 
-public interface UserRepository {
+import Model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
 }
