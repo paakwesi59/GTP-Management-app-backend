b7029a2802ac6d9d0b71ee8f53398840aa6be34d
emman <emmanuelassifuah9@gmail.com>
2024-07-11 09:32:54 +0000

    modified

---
+++ src/main/java/Service/EmailService.java
@@ -0,0 +1,20 @@
+package Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendInvitationEmail(String email, String username, String temporaryPassword) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Invitation to join the platform");
+        message.setText("Your username: " + username + "\nYour temporary password: " + temporaryPassword);
+        mailSender.send(message);
+    }
+}
