2796e88865abab90143c628fc819f38af9844187
Paa Kwesi <asmahpaakwesi59@gmail.com>
2024-07-11 04:35:40 -1100

    Fix userService injection issue in AuthenticationController

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,6 +1,9 @@
 package Service;
 
 import Model.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
     boolean existsByEmail(String email);
     User findByUsername(String username);
