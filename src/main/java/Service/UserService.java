6a19bf5e30e9c5951bf3c06057f74a4cbe727b19
RolandBissah10 <rolandbissah10@gmail.com>
2024-07-12 04:33:53 -0700

    login authentication done

--- src/main/java/Service/UserService.java
+++ src/main/java/Service/UserService.java
@@ -1,19 +1,3 @@
-//package Service;
-//
-//import Model.User;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public interface UserService {
-//    boolean existsByEmail(String email);
-//    User findByUsername(String username);
-//    boolean validatePassword(User user, String password);
-//    boolean processForgotPassword(String email);
-//    boolean resetPassword(String token, String newPassword);
-//    boolean changePassword(String username, String oldPassword, String newPassword);
-//
-//}
-
 package Service;
 
 import Model.Role;
